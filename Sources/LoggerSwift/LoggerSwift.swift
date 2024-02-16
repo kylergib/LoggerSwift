292864229fe84ca31f8cfe13cd11d73d43628747
kyle <kylergib@gmail.com>
2024-02-16 13:20:01 -0500

    made public

--- Sources/LoggerSwift/LoggerSwift.swift
+++ Sources/LoggerSwift/LoggerSwift.swift
@@ -2,57 +2,57 @@
 
 import Foundation
 
-class Logger {
+public class Logger {
     private var levelDict = ["info": 0, "debug": 1, "warning": 0, "error": 0, "critical": 0, "fatal": 0, "fine": 2, "finer": 3, "finest": 4]
     var currentLevel: String = "info"
     var currentClassName: String
     let formatter = DateFormatter()
 
-    init<T>(current: T) {
+    public init<T>(current: T) {
         currentClassName = String(describing: current.self)
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
     }
 
-    func info(_ message: String, function: String = #function, line: Int = #line) {
+    public func info(_ message: String, function: String = #function, line: Int = #line) {
         log("ℹ️", message, function: function, line: line)
     }
 
-    func debug(_ message: String, function: String = #function, line: Int = #line) {
+    public func debug(_ message: String, function: String = #function, line: Int = #line) {
         if levelDict[currentLevel]! >= 1 {
             log("🐞", message, function: function, line: line)
         }
     }
 
-    func warning(_ message: String, function: String = #function, line: Int = #line) {
+    public func warning(_ message: String, function: String = #function, line: Int = #line) {
 
         log("⚠️", message, function: function, line: line)
     }
 
-    func error(_ message: String, function: String = #function, line: Int = #line) {
+    public func error(_ message: String, function: String = #function, line: Int = #line) {
         log("❌", message, function: function, line: line)
     }
 
-    func critical(_ message: String, function: String = #function, line: Int = #line) {
+    public func critical(_ message: String, function: String = #function, line: Int = #line) {
         log("❗", message, function: function, line: line)
     }
 
-    func fatal(_ message: String, function: String = #function, line: Int = #line) {
+    public func fatal(_ message: String, function: String = #function, line: Int = #line) {
         log("☠️", message, function: function, line: line)
     }
 
-    func fine(_ message: String, function: String = #function, line: Int = #line) {
+    public func fine(_ message: String, function: String = #function, line: Int = #line) {
         if levelDict[currentLevel]! >= 2 {
             log("👍", message, function: function, line: line)
         }
     }
 
-    func finer(_ message: String, function: String = #function, line: Int = #line) {
+    public func finer(_ message: String, function: String = #function, line: Int = #line) {
         if levelDict[currentLevel]! >= 3 {
             log("👌", message, function: function, line: line)
         }
     }
 
-    func finest(_ message: String, function: String = #function, line: Int = #line) {
+    public func finest(_ message: String, function: String = #function, line: Int = #line) {
         if levelDict[currentLevel]! >= 4 {
             log("🌟", message, function: function, line: line)
         }
@@ -68,11 +68,11 @@ class Logger {
         
     }
 
-    func setLevel(level: String) {
+    public func setLevel(level: String) {
         currentLevel = levelDict.contains(where: { $0.key == level }) ? level : currentLevel
     }
 
-    func getLevel() -> String {
+    public func getLevel() -> String {
         return currentLevel
     }
 }
